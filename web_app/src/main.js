4b0640de4a5130db166dcf522c91aeda3510b8d4
424335727 <424335727@qq.com>
2019-05-19 20:17:23 +0800

    大象买

---
+++ web_app/src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import 'lib-flexible/flexible.js'
+import NutUI from '@nutui/nutui'
+import '@nutui/nutui/dist/nutui.css'
+
+NutUI.install(Vue)
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ web_app/src/views/index/index.vue
@@ -0,0 +1,32 @@
+<template>
+	<!-- 首页 -->
+	<div class="index">
+		<div class="search-box">
+      sadas
+		</div>
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+	name: "index",
+	components: {
+	},
+	props: [],
+	data () {
+		return {}
+	},
+	methods: {},
+	watch: {}
+};
+</script>
+<style lang="stylus" scoped>
+.index
+	.search-box
+		position sticky
+		width 100%
+		top 0
+		z-index 9
+		left 0
+</style>
